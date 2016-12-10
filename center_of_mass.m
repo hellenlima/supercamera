312e0c920114d1810e4d29a48d92a2e7f6613c39
Hellen Lima <hellenlima@poli.ufrj.br>
2016-12-10 00:33:53 -0200

    removed center_of_mass from root dir

--- center_of_mass.m
+++
@@ -1,9 +0,0 @@
-a = [0,0,0,1;0 0 1 0; 0, 1, 0, 0; 1, 0, 0, 0];
-mean_x = mean(a,1);
-mean_y = mean(a,2);
-
-cm_x = mean_x/sum(a(:));
-cm_x = sum(cm_x);
-
-cm_y = mean_y/sum(a(:));
-cm_y = sum(cm_y);
